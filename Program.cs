093d618ce2e24c1bb8f707d191ec6e1fd520baea
Bruno Selhorst <bselhorst88@gmail.com>
2024-10-08 12:11:32 -0500

    atualizacoes

--- Program.cs
+++ Program.cs
@@ -5,8 +5,8 @@ using TrilhaApiDesafio.Context;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<OrganizadorContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+var serverVersion = new MySqlServerVersion(new Version(10, 4, 32));
+builder.Services.AddDbContext<OrganizadorContext>(options => options.UseMySql(builder.Configuration.GetConnectionString("ConexaoPadrao"), serverVersion));
 
 builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
